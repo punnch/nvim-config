662bf0b99603ccf78a86e3c5c2053d35dfda4479
punnch <punnchxd@gmail.com>
2026-01-10 11:44:30 +0300

    feat: add new nvim keybinds

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,8 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " "
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
