80f764e55cab0ee19eeb5e346715483d4b6fe4b1
punnch <punnchxd@gmail.com>
2026-01-10 11:39:15 +0300

    fix: change 'treesitter' according to new version

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
   config = function()
-    local config = require('nvim-treesitter.configs') 
-    config.setup({
-      ensure_installed = { 'go', 'python' },
-      highlight = { enable = true },
-      indent = { enable = true },
+    require'nvim-treesitter'.install { 'go' }
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'go' },
+      callback = function() vim.treesitter.start() end,
     })
   end
-}
+} 
